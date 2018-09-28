9431b497188e29f6baabaa92e9b03d57ba21cfd0
stbz1996 <stbz1996@gmail.com>
2018-09-28 10:15:44 -0600

    BD

--- application/config/database.php
+++ application/config/database.php
@@ -28,7 +28,9 @@ $db['default'] = array(
 	'save_queries' => TRUE
 );
 */
+
 /************************************** Base de datos de Steven
+*/
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
@@ -49,9 +51,10 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
-); */
+);
+
 /*
-//************************************** Base de datos de desarrollo  */
+//************************************** Base de datos de desarrollo  
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'db4free.net',
@@ -73,3 +76,4 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+*/
