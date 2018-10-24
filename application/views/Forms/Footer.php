45fc53006a5d0310b256e6ffb492252c47308f4c
jorgegr1707 <jorgegr1707@gmail.com>
2018-10-23 18:34:18 -0600

    Modify PDF done

--- application/views/Forms/Footer.php
+++ application/views/Forms/Footer.php
@@ -2,6 +2,7 @@
 <script src='<?=base_url()?>js/generalFiles/jquery.easing.min.js'></script>
 <script src="<?=base_url()?>js/generalFiles/sweetalert.min.js"></script>
 <script src="<?=base_url()?>js/generalFiles/jspdf.min.js"></script>
+<script src="<?=base_url()?>js/generalFiles/jspdf.plugin.autotable.js"></script>
 <script src="<?=base_url()?>js/Forms/operations.js"></script>
 <script src="<?=base_url()?>js/Forms/index.js"></script>
 </body>
